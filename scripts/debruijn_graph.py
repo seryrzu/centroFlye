Simpler valid path calculation. Fix of cyclic shifts

scripts/debruijn_graph.py
@@ -1,4 +1,5 @@
 from collections import defaultdict, Counter
+from itertools import groupby
 import os
 
 import networkx as nx
@@ -32,12 +33,14 @@ class DeBruijnGraph:
             self.node_mapping[suffix] = suffix_node_ind
             self.rev_node_mapping[suffix_node_ind] = suffix
 
-        self.graph.add_edge(prefix_node_ind, suffix_node_ind,
-                            edge_kmer=kmer,
-                            length=1,
-                            coverages=[coverage],
-                            label=f'len=1\ncov={coverage}',
-                            color='black' if coverage < self.max_uniq_cov else 'red')
+        self.graph.add_edge(
+            prefix_node_ind,
+            suffix_node_ind,
+            edge_kmer=kmer,
+            length=1,
+            coverages=[coverage],
+            label=f'len=1\ncov={coverage}',
+            color='black' if coverage < self.max_uniq_cov else 'red')
 
     def add_kmers(self, kmers, coverage=None):
         for kmer in kmers:
@@ -59,6 +62,7 @@ class DeBruijnGraph:
                             for kmer, pos in index.items()
                             if len(pos) == 1}
             all_index[k] = index_unique
+        self.db_index = all_index
         return all_index
 
     def collapse_nonbranching_paths(self):
@@ -87,12 +91,12 @@ class DeBruijnGraph:
                 new_coverages.sort()
                 new_edge_len = len(new_coverages)
                 new_edge_med_cov = np.median(new_coverages)
-                self.graph.add_edge(in_node, out_node,
-                                    edge_kmer=new_kmer,
-                                    coverages=new_coverages,
-                                    length=new_edge_len,
-                                    label=f'len={new_edge_len}\ncov={new_edge_med_cov}',
-                                    color='black' if new_edge_med_cov < self.max_uniq_cov else 'red')
+                self.graph.add_edge(
+                    in_node, out_node, edge_kmer=new_kmer,
+                    coverages=new_coverages, length=new_edge_len,
+                    label=f'len={new_edge_len}\ncov={new_edge_med_cov}',
+                    color='black'
+                    if new_edge_med_cov < self.max_uniq_cov else 'red')
                 self.graph.remove_node(node)
 
     def get_edges(self):
@@ -104,84 +108,75 @@ class DeBruijnGraph:
         return contigs, coverages
 
     def get_path(self, list_edges):
-        db_edges = list(self.graph.edges())
         path = ''
         path += self.graph.get_edge_data(*list_edges[0])['edge_kmer']
         for edge in list_edges[1:]:
             new_edge_seq = self.graph.get_edge_data(*edge)['edge_kmer']
-            # print(len(path[-(self.k-1):]), path[-(self.k-1):])
-            # print(len(new_edge_seq[:self.k-1]), new_edge_seq[:self.k-1])
             assert path[-(self.k-1):] == new_edge_seq[:self.k-1]
             path += new_edge_seq[self.k-1:]
 
         # process cycled path
         if list_edges[0][0] == list_edges[-1][1]:
             path = path[:-(self.k-1)]
-            doubled_path = path*2
-            min_shift, min_path = 0, path
-            for i in range(1, len(path)):
-                cur_path = doubled_path[i:i+len(path)]
-                if cur_path < min_path:
-                    min_shift = i
-                    min_path = cur_path
-            path = min_path
         return path
 
     def get_contigs(self):
-        def get_valid_outpaths_from_edge(edge, taken_edges=set()):
-            out_node = edge[1]
-            out_degree_out_node = self.graph.out_degree(out_node)
-            paths = [[edge]]
-            if out_degree_out_node == 1:
-                out_edge = list(self.graph.out_edges(out_node, keys=True))[0]
-                if out_edge not in taken_edges:
-                    passed_taken_edges = taken_edges.copy()
-                    passed_taken_edges.add(edge)
-                    paths = get_valid_outpaths_from_edge(out_edge,
-                                                         taken_edges=passed_taken_edges)
-                    paths = paths.copy()
-                    for i in range(len(paths)):
-                        paths[i].append(edge)
-            return paths
+        def get_longest_valid_outpaths(graph):
+            def get_valid_outpath_edge(edge, taken_edges=set()):
+                path = [edge]
+                out_node = edge[1]
+                out_degree_out_node = graph.out_degree(out_node)
+                if out_degree_out_node == 1:
+                    out_edge = list(
+                        graph.out_edges(
+                            out_node, keys=True))[0]
+                    if out_edge not in taken_edges:
+                        taken_edges.add(edge)
+                        out_edge_path = \
+                            get_valid_outpath_edge(out_edge,
+                                                   taken_edges=taken_edges)
+                        path += out_edge_path
+                return path
 
-        def get_valid_inpaths_from_edge(edge, taken_edges=set()):
-            in_node = edge[0]
-            in_degree_in_node = self.graph.in_degree(in_node)
+            outpaths = {}
+            for edge in graph.edges(keys=True):
+                if edge not in outpaths:
+                    outpaths[edge] = get_valid_outpath_edge(edge)
+                    for i, e in enumerate(outpaths[edge][1:]):
+                        outpaths[e] = outpaths[edge][i+1:]
+            return outpaths
 
-            paths = [[edge]]
-            if in_degree_in_node == 1:
-                in_edge = list(self.graph.in_edges(in_node, keys=True))[0]
-                if in_edge not in taken_edges:
-                    passed_taken_edges = taken_edges.copy()
-                    passed_taken_edges.add(edge)
-                    paths = get_valid_inpaths_from_edge(in_edge,
-                                                        taken_edges=passed_taken_edges)
-                    paths = paths.copy()
-                    for i in range(len(paths)):
-                        paths[i].append(edge)
-            return paths
+        self.collapse_nonbranching_paths()
+        outpaths = get_longest_valid_outpaths(self.graph)
 
-        # Getting valid in- and out-paths for each edge
-        edge_outpaths, edge_inpaths = {}, {}
-        for edge in self.graph.edges(keys=True):
-            edge_outpaths[edge] = get_valid_outpaths_from_edge(edge)
-            edge_inpaths[edge] = get_valid_inpaths_from_edge(edge)
-        for edge in self.graph.edges(keys=True):
-            edge_inpaths[edge] = [tuple(path) for path in edge_inpaths[edge]]
-            edge_outpaths[edge] = [tuple(path[::-1]) for path in edge_outpaths[edge]]
+        rev_graph = self.graph.reverse()
+        rev_inpaths = get_longest_valid_outpaths(rev_graph)
+        inpaths = {}
+        for rev_edge in rev_inpaths:
+            edge = (rev_edge[1], rev_edge[0], rev_edge[2])
+            inpaths[edge] = rev_inpaths[rev_edge][::-1]
+            inpaths[edge] = [(e[1], e[0], e[2]) for e in inpaths[edge]]
 
-        # Unite all such paths and delete duplicates
-        all_paths = []
-        for edge in self.graph.edges(keys=True):
-            all_paths += edge_inpaths[edge] + edge_outpaths[edge]
-        all_paths = list(set(all_paths))
+        valid_paths = []
+        for edge in outpaths:
+            valid_path = inpaths[edge]
+            seen_edges = set(inpaths[edge])
+            for e in outpaths[edge][1:]:
+                if e not in seen_edges:
+                    valid_path.append(e)
+                    seen_edges.add(e)
+                else:
+                    break
+            valid_path = tuple(valid_path)
+            valid_paths.append(valid_path)
+
+        valid_paths = list(set(valid_paths))
 
         # Select only paths that are not a subpath of other paths
         selected_paths = []
-        # doubled_paths = [path + path for path in all_paths]
-        for path1 in all_paths:
+        for path1 in valid_paths:
             has_duplicate = False
-            for path2 in all_paths:
+            for path2 in valid_paths:
                 if path1 == path2:
                     continue
                 for i in range(len(path2)-len(path1)+1):
@@ -192,27 +187,57 @@ class DeBruijnGraph:
                     break
             if not has_duplicate:
                 selected_paths.append(path1)
-
-        # check that all edges of that graph are incorporated in selected paths
-        # edges_in_selected_paths = []
-        # for path in selected_paths:
-        #     edges_in_selected_paths += path
-        # assert len(set(self.graph.edges(keys=True)) - set(edges_in_selected_paths)) == 0
+        valid_paths = selected_paths
 
         contigs = []
-        for path in selected_paths:
+        for path in valid_paths:
             contigs.append(self.get_path(path))
         contigs = list(set(contigs))
-        return contigs, selected_paths
+        return contigs, valid_paths
+
+    def map_reads(self, monomer_strings, verbose=True):
+        print("Indexing started")
+        self.index_edges()
+        print("Indexing completed")
+        mapping = {}
+        db_edges = list(self.graph.edges(keys=True))
+        for i, (r_id, string) in enumerate(monomer_strings.items()):
+            if verbose:
+                print(i+1, len(monomer_strings))
+
+            split_strings = list(filter(lambda string: len(string), string.split('=')))
+            split_lens = [0] + [len(split_string) for split_string in split_strings]
+            cum_split_lens = np.cumsum(split_lens)
+            read_coords = []
+            for split_ind, split_string in enumerate(split_strings):
+                for i in range(len(split_string)-self.k+1):
+                    kmer = split_string[i:i+self.k]
+                    if kmer in self.db_index[len(kmer)]:
+                        read_coords.append(self.db_index[len(kmer)][kmer])
+
+            path = [x[0] for x in read_coords]
+            path = [x[0] for x in groupby(path)]
+            path = [db_edges[edge_ind] for edge_ind in path]
+
+            valid_path = True
+            for e1, e2 in zip(path[:-1], path[1:]):
+                if e1[1] != e2[0]:
+                    valid_path = False
+                    break
+            if len(read_coords):
+                mapping[r_id] = (read_coords[0], read_coords[-1], valid_path, path)
+            else:
+                mapping[r_id] = None
+        return mapping
 
 
-def get_all_kmers(strings, k):
+def get_all_kmers(strings, k, gap_symb='?'):
     all_kmers = Counter()
     read_kmer_locations = defaultdict(list)
     for r_id, string in strings.items():
         for i in range(len(string)-k+1):
             kmer = string[i:i+k]
-            if '=' not in kmer:
+            if gap_symb not in kmer:
                 all_kmers[kmer] += 1
                 read_kmer_locations[kmer].append((r_id, i))
     return all_kmers, read_kmer_locations
@@ -245,38 +270,40 @@ def get_paths_thru_complex_nodes(db, strings, min_mult=2):
 
 
 def get_frequent_kmers(strings, k, min_mult=5):
-    all_kmers, read_kmer_locations = get_all_kmers(strings, k)       
+    all_kmers, read_kmer_locations = get_all_kmers(strings, k)
     frequent_kmers = {kmer: cnt for kmer, cnt in all_kmers.items()
                       if cnt >= min_mult}
-    frequent_kmers_read_pos = {kmer: read_kmer_locations[kmer] for kmer in frequent_kmers}
+    frequent_kmers_read_pos = {
+        kmer: read_kmer_locations[kmer] for kmer in frequent_kmers}
     return frequent_kmers, frequent_kmers_read_pos
 
 
-def iterative_graph(strings, min_k, max_k, outdir, min_mult=5, step=1, starting_graph=None):
+def iterative_graph(strings, min_k, max_k, outdir,
+                    min_mult=5, step=1, starting_graph=None, verbose=True):
     smart_makedirs(outdir)
-    
     dbs, all_contigs = {}, {}
     all_frequent_kmers, all_frequent_kmers_read_pos = {}, {}
     input_strings = strings.copy()
     complex_kp1mers = {}
-    
+
     if starting_graph is not None:
         contigs, contig_paths = starting_graph.get_contigs()
         for i in range(len(contigs)):
             for j in range(min_mult):
                 input_strings[f'contig_k{min_k}_i{i}_j{j}'] = contigs[i]
-        
+
         complex_kp1mers = get_paths_thru_complex_nodes(starting_graph, strings)
 
-
     for k in range(min_k, max_k+1, step):
-        print(f'\nk={k}')
-        frequent_kmers, frequent_kmers_read_pos = get_frequent_kmers(input_strings, k=k, min_mult=min_mult)
+        frequent_kmers, frequent_kmers_read_pos = \
+            get_frequent_kmers(input_strings, k=k, min_mult=min_mult)
         frequent_kmers.update(complex_kp1mers)
-        print(f'#frequent kmers = {len(frequent_kmers)}')
+        if verbose:
+            print(f'\nk={k}')
+            print(f'#frequent kmers = {len(frequent_kmers)}')
         all_frequent_kmers[k] = frequent_kmers
         all_frequent_kmers_read_pos[k] = frequent_kmers_read_pos
-        
+
         db = DeBruijnGraph(k=k)
         db.add_kmers(frequent_kmers, coverage=frequent_kmers)
 
@@ -287,13 +314,12 @@ def iterative_graph(strings, min_k, max_k, outdir, min_mult=5, step=1, starting_
                 print(len(cc))
             # break
         dbs[k] = db
-        
+
         dot_file = os.path.join(outdir, f'db_k{k}.dot')
         # pdf_file = os.path.join(outdir, f'db_k{k}.pdf')
         nx.drawing.nx_pydot.write_dot(db.graph, dot_file)
         # os.system(f"dot -Tpdf {dot_file} -o {pdf_file}")
-        
-        
+
         contigs, contig_paths = db.get_contigs()
         all_contigs[k] = contigs
 
@@ -301,7 +327,7 @@ def iterative_graph(strings, min_k, max_k, outdir, min_mult=5, step=1, starting_
         for i in range(len(contigs)):
             for j in range(min_mult):
                 input_strings[f'contig_k{k}_i{i}_j{j}'] = contigs[i]
-        
+
         complex_kp1mers = get_paths_thru_complex_nodes(db, strings)
 
     return all_contigs, dbs, all_frequent_kmers, all_frequent_kmers_read_pos