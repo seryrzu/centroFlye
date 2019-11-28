2a1ab9ef3029b37a682ad6e978f6f57632b82379
Andrey Bzikadze <seryrzu@gmail.com>
2019-11-28 08:35:44 -0800

    monostrings + error correction

--- scripts/mono_error_correction.py
+++ scripts/mono_error_correction.py
@@ -23,7 +23,6 @@ def filter_lowercaserich_reads(monoreads, max_lowercase=0.1):
 
 
 def trim_read(monoread, max_gap, ma_window, gap_symb='?'):
-    monoread = deepcopy(monoread)
     is_gap = [c == gap_symb for c in monoread]
     ma = get_ma(is_gap, N=ma_window)
     left = 0
@@ -34,21 +33,19 @@ def trim_read(monoread, max_gap, ma_window, gap_symb='?'):
         right -= 1
     monoread.trim_read(left, right+ma_window+1)
     monoread.strip()
-    trimmed_read = trimmed_read.strip(gap_symb)
-    trimmed_length = left + len(ma)-(right+1+ma_window)
-    return monoread, trimmed_length
+    return monoread
 
 
 def trim_reads(monoreads, max_gap=0.2, ma_window=30):
     trimmed_reads = {}
     for r_id, monoread in monoreads.items():
-        trimmed_read, trimmed_length = \
+        trimmed_read = \
             trim_read(monoread, max_gap=max_gap, ma_window=ma_window)
         trimmed_reads[r_id] = trimmed_read
     return trimmed_reads
 
 
-def cut_gaprich_reads(monoreads, max_gap=0.05, min_len=100, gap_symb='?'):
+def cut_gaprich_reads(monoreads, max_gap=0.05, min_length=100, gap_symb='?'):
     processed_reads = {}
     cut_cnt = 0
     total_parts_cnt = 0
@@ -59,72 +56,77 @@ def cut_gaprich_reads(monoreads, max_gap=0.05, min_len=100, gap_symb='?'):
         gap_cnt = Counter(monoread)[gap_symb]
         gap_prop = gap_cnt / len(monoread)
         if gap_prop <= max_gap:
-            processed_reads[r_id] = monoread
+            processed_reads[(r_id, 0)] = monoread
         else:
-            parts = monoread.split(gap_symb)
-            part_cnt = 0
-            for part in parts:
-                if len(part) >= min_len:
-                    processed_reads[f'{r_id}-{part_cnt}'] = part
-                    part_cnt += 1
+            parts = monoread.split(c=gap_symb, min_length=min_length)
+            part_cnt = sum(len(part.string) > 0 for part in parts.values())
             if part_cnt:
                 cut_cnt += 1
                 total_parts_cnt += part_cnt
+            processed_reads.update(parts)
     return processed_reads, cut_cnt, total_parts_cnt
 
 
-def correct_gaps(monomer_strings, max_gap=0.3, nhor=1,
+def correct_gaps(monostrings, max_gap=0.3, nhor=1,
                  k=3, min_mult=5000,
                  gap_symb='?'):
     frequent_kmers, frequent_kmers_read_pos = \
-        get_frequent_kmers(monomer_strings, k=k, min_mult=min_mult)
+        get_frequent_kmers(monostrings, k=k, min_mult=min_mult)
     db = DeBruijnGraph(k=k)
     db.add_kmers(frequent_kmers, coverage=frequent_kmers)
 
     hors, _ = db.get_contigs()
     hors = [min_cyclic_shift(hor) for hor in hors]
     corrected_strings = {}
-    for r_id, monomer_string in monomer_strings.items():
-        corrected_string = list(monomer_string)
+    for r_id, monostring in monostrings.items():
         for single_hor in hors:
             for i_nhor in range(1, nhor+1):
                 hor = single_hor * i_nhor
                 hor_len = len(hor)
-                for i in range(len(monomer_string)-hor_len+1):
-                    kmer = monomer_string[i:i+hor_len]
+                for i in range(len(monostring)-hor_len+1):
+                    kmer = monostring[i:i+hor_len]
                     gap_cnt = Counter(kmer)[gap_symb]
                     if gap_cnt == 0 or gap_cnt / hor_len > max_gap:
                         continue
                     hd, _ = hamming_distance(kmer, hor,
                                              match_char=set(gap_symb))
                     if hd == 0:
-                        corrected_string[i:i+hor_len] = list(hor)
-
-        corrected_strings[r_id] = ''.join(corrected_string)
-    return corrected_strings
+                        monostring[i:i+hor_len] = list(hor)
+    return monostrings
 
 
-def error_correction(monoreads, verbose=False, hor_correction=True):
-    filtered_reads = filter_lowercaserich_reads(monoreads)
-    # trimmed_monoreads = trim_reads(filtered_reads)
-    # cut_monoreads, cut_cnt, total_parts_cnt = \
-    #     cut_gaprich_reads(trimmed_monoreads)
-    # corrected_reads = cut_monoreads
-    # if hor_correction:
-    #     hor_corrected = correct_gaps(cut_monoreads)
-    #     corrected_reads = hor_corrected
+def error_correction(monoreads, inplace=True, verbose=False, hor_correction=True):
+    if not inplace:
+        monoreads = deepcopy(monoreads)
+
     if verbose:
         print('Stats for uncorrected reads')
         get_stats(monoreads, verbose=True, return_stats=False)
+    filtered_reads = filter_lowercaserich_reads(monoreads)
+
+    if verbose:
         print('\nStats for filtered reads')
         get_stats(filtered_reads, verbose=True, return_stats=False)
-    #     print('\nStats for trimmed+filtered reads')
-    #     get_stats(trimmed_monoreads, verbose=True, return_stats=False)
-    #     print('\nStats for cut_gaprich_reads+trimmed+filtered reads')
-    #     print(f'# cut reads = {cut_cnt}')
-    #     print(f'# total cut parts = {total_parts_cnt}')
-    #     get_stats(cut_monoreads, verbose=True, return_stats=False)
-    #     if hor_correction:
-    #         print('\nStats for hor_corrected+cut_gaprich_reads+trimmed+filtered reads')
-    #         get_stats(hor_corrected, verbose=True, return_stats=False)
-    # return corrected_reads
+
+    trimmed_monoreads = trim_reads(filtered_reads)
+    if verbose:
+        print('\nStats for trimmed+filtered reads')
+        get_stats(trimmed_monoreads, verbose=True, return_stats=False)
+
+    cut_monoreads, cut_cnt, total_parts_cnt = \
+        cut_gaprich_reads(trimmed_monoreads)
+    if verbose:
+        print('\nStats for cut_gaprich_reads+trimmed+filtered reads')
+        print(f'# cut reads = {cut_cnt}')
+        print(f'# total cut parts = {total_parts_cnt}')
+        get_stats(cut_monoreads, verbose=True, return_stats=False)
+    corrected_reads = cut_monoreads
+
+    if hor_correction:
+        hor_corrected = correct_gaps(cut_monoreads)
+        corrected_reads = hor_corrected
+        if verbose:
+            print('\nStats for hor_corrected+cut_gaprich_reads+trimmed+filtered reads')
+            get_stats(hor_corrected, verbose=True, return_stats=False)
+
+    return corrected_reads
--- scripts/sd_parser_83640e3.py
+++ scripts/sd_parser_83640e3.py
@@ -9,24 +9,83 @@ from utils.bio import read_bio_seqs, compress_homopolymer
 
 
 class MonoString:
-    def __init__(self, gap_symb):
-        self.string = []
-        self.mono2nucl = {}
+    def __init__(self, name,
+                 string=list(), mono2nucl=dict(),
+                 gap_symb='?', strand='+'):
+        self.name = name
+        self.string = string.copy()
+        self.mono2nucl = mono2nucl.copy()
         self.gap_symb = gap_symb
-        self.trimmed_len = 0
-        self.strand = '+'
+        self.strand = strand
 
-    def add_gap(self, length):
-        self.string += [self.gap_symb] * length
+    @classmethod
+    def FromSDRecord(cls, name, monomers, starts, ends, reliability,
+                     max_gap, mean_monomer_len, gap_symb):
 
-    def add_monomer(self, monomer, st, en):
-        self.mono2nucl[len(self.string)] = (monomer, st, en)
-        self.string.append(monomer)
+        monostring = cls(name=name)
+
+        if reliability[0] == '+':
+            monostring.add_monomer(monomer=monomers[0],
+                                   st=starts[0],
+                                   en=ends[0])
+        else:
+            monostring.add_gap(1)
+
+        triple1 = zip(monomers[:-1], starts[:-1], ends[:-1])
+        triple2 = zip(monomers[1:], starts[1:], ends[1:])
+        for (m1, s1, e1), (m2, s2, e2), rel2 in \
+                zip(triple1, triple2, reliability[1:]):
+            gap_len = s2 - e1
+            if gap_len > max_gap:
+                int_gap_len = int(round(gap_len / mean_monomer_len))
+                monostring.add_gap(int_gap_len)
+            if rel2 == '+':
+                monostring.add_monomer(m2, s2, e2)
+            else:
+                monostring.add_gap(1)
+
+        monostring.assert_validity()
+        monostring.strip()
+        monostring.check_reverse()
+        return monostring
+
+    def tostring(self):
+        return ''.join(self.string)
+
+    def __len__(self):
+        return self.string.__len__()
+
+    def __getitem__(self, sub):
+        if isinstance(sub, slice):
+            sublist = self.string[sub.start:sub.stop:sub.step]
+            sublist = ''.join(sublist)
+            return sublist
+        return self.string[sub]
+
+    def __setitem__(self, sub, value):
+        if isinstance(sub, slice):
+            if isinstance(value, str):
+                value = list(value)
+            self.string[sub.start:sub.stop:sub.step] = value
+        else:
+            self.string[sub] = value
+
 
     def assert_validity(self):
         for coord, (c, _, _) in self.mono2nucl.items():
-            # print(coord, c, self.string[coord])
+            if coord < 0:
+                print(coord, c)
+            if coord >= len(self.string):
+                print(coord, c, len(self.stirng))
             assert c == self.string[coord]
+
+    def add_monomer(self, monomer, st, en):
+        self.mono2nucl[len(self.string)] = \
+            (monomer, st, en)
+        self.string.append(monomer)
+
+    def add_gap(self, length):
+        self.string += [self.gap_symb] * length
 
     def check_reverse(self):
         perc_lower_case = np.mean([c.islower() for c in self.string
@@ -57,57 +116,42 @@ class MonoString:
             j -= 1
         self.trim_read(i, j+1)
 
-    def tostring(self):
-        return ''.join(self.string)
+    def split(self, c, min_length):
+        resulting_monostrings = {}
+        split_strings = self.tostring().split(c)
 
-    def __len__(self):
-        return self.string.__len__()
+        pos = list(self.mono2nucl)
+        coord_pos = 0
+        cumm_len = 0
 
-    def __getitem__(self, sub):
-        if isinstance(sub, slice):
-            sublist = self.string[sub.start:sub.stop:sub.step]
-            sublist = ''.join(sublist)
-            return sublist
-        return self.string[sub]
+        for i, split_string in enumerate(split_strings):
+            if len(split_string) < min_length:
+                cumm_len += len(split_string) + 1
+                continue
+
+            split_mono2nucl = {}
+            while coord_pos < len(pos) and pos[coord_pos] < cumm_len:
+                coord_pos += 1
+
+            while pos[coord_pos] < + len(split_string):
+                p = pos[coord_pos]
+                assert split_string[p - cumm_len] == self.mono2nucl[p][0]
+                split_mono2nucl[p - cumm_len] = self.mono2nucl[p]
+                coord_pos += 1
+
+            new_monostring = MonoString(name=self.name,
+                                        string=list(split_string),
+                                        mono2nucl=split_mono2nucl,
+                                        gap_symb=self.gap_symb,
+                                        strand=self.strand)
+            new_monostring.assert_validity()
+
+            resulting_monostrings[(self.name, i)] = new_monostring
+            cumm_len += len(split_string) + 1
+        return resulting_monostrings
 
 
 class SD_Report:
-    class SD_Record:
-        def __init__(self, r_id, pandas_df,
-                     max_gap, mean_monomer_len, gap_symb):
-            self.r_id = r_id
-            self.monomers = pandas_df.monomer.to_list()
-            self.r_st = pandas_df.r_st.to_list()
-            self.r_en = pandas_df.r_en.to_list()
-            self.triples = list(zip(self.monomers, self.r_st, self.r_en))
-            self.score = pandas_df.score.to_list()
-            self.alt_call = pandas_df.alt_call.to_list()
-
-            self.string = MonoString(gap_symb=gap_symb)
-
-            if self.alt_call[0] == '+':
-                monomer, st, en = self.triples[0]
-                self.string.add_monomer(monomer=monomer, st=st, en=en)
-            else:
-                self.string.add_gap(1)
-
-            self.gaps = []
-            for tr1, tr2, rel2 in zip(self.triples[:-1],
-                                      self.triples[1:],
-                                      self.alt_call[1:]):
-                gap_len = tr2[1] - tr1[2]
-                if gap_len > max_gap:
-                    self.gaps.append((tr1[2], tr2[1]))
-                    int_gap_len = int(round(gap_len / mean_monomer_len))
-                    self.string.add_gap(int_gap_len)
-                if rel2 == '+':
-                    self.string.add_monomer(*tr2)
-                else:
-                    self.string.add_gap(1)
-
-            self.string.strip()
-            self.string.check_reverse()
-
     def __init__(self, SD_report_fn, monomers_fn, max_gap=100, gap_symb='?'):
         self.gap_symb = gap_symb
         monomers = read_bio_seqs(monomers_fn)
@@ -120,26 +164,26 @@ class SD_Report:
             self.monomer_names_map[monomer_name] = ucode
             self.monomer_names_map[monomer_name + "'"] = lcode
 
-        self.records = {}
+        self.monostrings = {}
         df = pd.read_csv(SD_report_fn, sep='\t',
                          header=None,
                          names=['r_id', 'monomer',
                                 'r_st', 'r_en',
                                 'score',
-                                'alt_call'
+                                'reliability'
                                 ])
         df.monomer = df.monomer.apply(lambda x: self.monomer_names_map[x])
         df = df.groupby('r_id')
         for r_id, group in df:
-            self.records[r_id] = \
-                self.SD_Record(r_id, group,
-                               max_gap=max_gap,
-                               mean_monomer_len=mean_monomer_len,
-                               gap_symb=self.gap_symb)
-
-    def get_monomer_strings(self):
-        return {r_id: record.string
-                for r_id, record in self.records.items() if len(record.string)}
+            self.monostrings[r_id] = \
+                MonoString.FromSDRecord(name=r_id,
+                                        monomers=group.monomer.to_list(),
+                                        starts=group.r_st.to_list(),
+                                        ends=group.r_en.to_list(),
+                                        reliability=group.reliability.to_list(),
+                                        max_gap=max_gap,
+                                        mean_monomer_len=mean_monomer_len,
+                                        gap_symb=self.gap_symb)
 
 
 def get_ngap_symbols(monostrings, compr_hmp=False, gap_symb='?'):
