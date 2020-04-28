f6b9ac71964d5842ce00077d56fc44309a5ac1d9
seryrzu <seryrzu@gmail.com>
2020-04-28 12:48:07 -0700

    monomers added doc

--- scripts/monomers/monomers.py
+++ scripts/monomers/monomers.py
@@ -6,13 +6,46 @@ logger = logging.getLogger("centroFlye.monomers.monomers")
 
 
 class MonomerDB:
+    """
+    Database of monomers is usually read from fasta file
+
+    Attributes
+    ----------
+    seqs : list of str
+        List of all monomers in the database
+    names2ident : dict
+        Mapping from names (str) into identificators (indexes of seqs, int)
+    ident2names : dict
+        Mapping from identificators (indexes of seqs, int) into names (str)
+        For each name ident2names[names2ident[name]] == name
+        For each index (0 <= index < len(seqs))
+            names2ident[index2names[index]] == index
+    """
+
     def __init__(self, seqs, names2ident, ident2names):
+        """
+        Constuctor for MonomerDB class
+
+        Parameters are same as attributes of the class
+        """
+
         self.seqs = seqs
         self.names2ident = names2ident
         self.ident2names = ident2names
 
     @classmethod
     def from_fasta_file(cls, fn):
+        """
+        Class method to construct MonomerDB from a fasta file
+
+        Sequences in MonomerDB will be sorted identically
+        to the order in the file
+
+        Parameters
+        ----------
+        fn : str
+            Name of the fasta file with monomers
+        """
         logger.info(f'Creating Monomer DataBase from {fn}')
         monomers = read_bio_seqs(fn)
         names2ident = {}
@@ -32,15 +65,52 @@ class MonomerDB:
         return monomer_db
 
     def get_seq_by_index(self, index):
+        """
+        Method to get sequence by index
+
+        Parameters
+        ----------
+        index : int
+
+        Returns
+        -------
+        str
+        """
+        assert 0 <= index < len(self.seqs)
         return self.seqs[index]
 
     def get_seq_by_name(self, name):
+        """
+        Method to get sequence by name
+
+        Parameters
+        ----------
+        name : str
+
+        Returns
+        -------
+        str
+        """
         index = self.names2ident[name]
         return self.seqs[index]
 
     def get_names(self):
+        """
+        Method to get names of all monomers in the same order as in self.seqs
+
+        Returns
+        -------
+        list
+        """
         names_generator = self.names2ident.keys()
         return list(names_generator)
 
     def get_total_monomers(self):
+        """
+        Method to the total number of monomers
+
+        Returns
+        -------
+        int
+        """
         return len(self.seqs)
