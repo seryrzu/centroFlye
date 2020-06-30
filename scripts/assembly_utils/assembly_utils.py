1b239b621b81b4b9beab61278761cc68105b0c4f
seryrzu <seryrzu@gmail.com>
2020-06-29 21:52:02 -0700

    map_monoreads_to_monoassembly

--- scripts/assembly_utils/assembly_utils.py
+++ scripts/assembly_utils/assembly_utils.py
@@ -2,15 +2,44 @@
 # This file is a part of centroFlye program.
 # Released under the BSD license (see LICENSE file)
 
-from collections import Counter
 import os
 
-import numpy as np
-
-import matplotlib as mpl
-mpl.use('Agg')
-import matplotlib.pyplot as plt
-
 from utils.os_utils import smart_makedirs
+from utils.various import fst_iterable
+
+from mapping.mapping import map_queries, get_coverage
 
 
+def map_monoreads_to_monoassembly(monoreads_set, monoassembly_set,
+                                  outdir=None):
+    monoassembly = fst_iterable(monoassembly_set.values())
+    raw_monoassembly = monoassembly.raw_monostring
+    gap_symb = monoassembly.gap_symb
+    locations = map_queries(queries=monoreads_set,
+                            targets=[raw_monoassembly],
+                            max_nloc_target=1,
+                            max_ntarget_locs=1,
+                            neutral_symbs=set(gap_symb),
+                            max_dist=0)
+    locations = locations[0]
+
+    smart_makedirs(outdir)
+    locations_fn = os.path.join(outdir, 'locations.tsv')
+    with open(locations_fn, 'w') as f:
+        print(f'r_id\tmono_st\tmono_en\tr_nucl_st\tr_nucl_en\t'
+              f'a_nucl_st\ta_nucl_en', file=f)
+        for r_id, loc in locations.items():
+            assert len(loc) == 1
+            s, e = loc[0]
+            a_nucl_s = monoassembly.monoinstances[s].st
+            a_nucl_e = monoassembly.monoinstances[e].en
+            monoread = monoreads_set[r_id]
+            r_nucl_s = monoread.monoinstances[0].st
+            r_nucl_e = monoread.monoinstances[-1].en
+            print(f'{r_id}\t{s}\t{e}\t{r_nucl_s}\t{r_nucl_e}\t'
+                  f'{a_nucl_s}\t{a_nucl_e}', file=f)
+
+    get_coverage(locations=locations,
+                 target_len=len(raw_monoassembly),
+                 outdir=outdir,
+                 title='Coverage of monoassembly with monoreads')
